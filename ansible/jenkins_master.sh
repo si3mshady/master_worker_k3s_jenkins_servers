0b2084fe25b04468f65b37092dc9ecaeb2c6fc57
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2021-07-19 19:20:54 -0500

    savePoint

--- ansible/jenkins_master.sh
+++ ansible/jenkins_master.sh
@@ -2,8 +2,11 @@
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
-sudo yum install -y jenkins java-11-openjdk-devel 
+sudo yum install -y jenkins 
+sudo yum -y install java-1.8.0-openjdk
 sudo systemctl daemon-reload
+sudo systemctl start jenkins
 sudo yum upgrade -y
 
 
+# https://github.com/si3mshady/DevOps-Demos/tree/master/setup-jenkins-slave
